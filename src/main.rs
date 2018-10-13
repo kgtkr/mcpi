8e4090d0ed435281ffb7ab400f0e76eaaf916f06
kgtkr <kgtkr.jp@gmail.com>
2018-10-13 21:00:35 +0900

    並列化

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 extern crate rand;
 
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    let mut i = 0i64;
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..10 {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let mut i = 0;
+            let mut c = 0;
+            loop {
+                let x = rand::random::<f64>();
+                let y = rand::random::<f64>();
+                if x * x + y * y < 1.0 {
+                    c += 1;
+                }
+                i += 1;
+                if i % 1000000 == 0 {
+                    tx.send((i, c)).unwrap();
+                    i = 0;
+                    c = 0;
+                }
+            }
+        });
+    }
+
+    let mut i = 0;
     let mut c = 0;
     loop {
-        let x = rand::random::<f64>();
-        let y = rand::random::<f64>();
-        if x * x + y * y < 1.0 {
-            c += 1;
-        }
-        i += 1;
-        if i % 1000000 == 0 {
-            println!("{} {}", i, (c * 4) as f64 / i as f64);
-        }
+        let (x, y) = rx.recv().unwrap();
+        i += x;
+        c += y;
+        println!("{} {}", i, (c * 4) as f64 / i as f64);
     }
 }
