4d39b25d5897a61eb94f5ba87fe67c3059de8ffd
kgtkr <kgtkr.jp@gmail.com>
2018-10-14 14:32:53 +0900

    オーバーフロー対策、フォーマット

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate rand;
+extern crate separator;
+use separator::Separatable;
 
 use std::sync::mpsc;
 use std::thread;
@@ -8,8 +10,8 @@ fn main() {
     for _ in 0..10 {
         let tx = tx.clone();
         thread::spawn(move || {
-            let mut i = 0;
-            let mut c = 0;
+            let mut i = 0i64;
+            let mut c = 0i64;
             loop {
                 let x = rand::random::<f64>();
                 let y = rand::random::<f64>();
@@ -17,7 +19,7 @@ fn main() {
                     c += 1;
                 }
                 i += 1;
-                if i % 1000000 == 0 {
+                if i % 10000000 == 0 {
                     tx.send((i, c)).unwrap();
                     i = 0;
                     c = 0;
@@ -26,12 +28,12 @@ fn main() {
         });
     }
 
-    let mut i = 0;
-    let mut c = 0;
+    let mut i = 0i64;
+    let mut c = 0i64;
     loop {
         let (x, y) = rx.recv().unwrap();
         i += x;
         c += y;
-        println!("{} {}", i, (c * 4) as f64 / i as f64);
+        println!("{:>16} {}", i.separated_string(), (c * 4) as f64 / i as f64);
     }
 }
